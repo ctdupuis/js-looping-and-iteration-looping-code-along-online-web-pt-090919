1f793c7e8d946e71c5dfd2a6a4813b5a867a94e4
ctdupuis <ctdupuis7@gmail.com>
2020-03-18 20:35:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (names, eventName) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cards;
+}
+
+function countDown (integer) {
+    let i = integer;
+    while (integer > 0) {
+        console.log( integer )
+        integer = integer - 1
+    }
+    console.log( integer );
+}
